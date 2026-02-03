8cd3281c68e6db3fbb097ca354627b442d56b67e
tugumeandree <andrewtugume2@gmail.com>
2026-02-04 01:47:24 +0300

    pay out date fix

--- app/client-portal/page.tsx
+++ app/client-portal/page.tsx
@@ -492,9 +492,6 @@ export default function ClientPortal() {
                         </span>
                       </div>
                     </div>
-                    <div className="text-sm text-gray-900 font-bold mt-3 bg-yellow-50 border-l-4 border-yellow-400 p-3 rounded">
-                      *Fees cover transaction costs, taxes, service charges, and operational expenses
-                    </div>
                   </div>
                 </div>
               )}
